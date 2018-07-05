4fe13dd9fa042fa036970fb19c16075895302cd1
jgrondier <juliengrondier@gmail.com>
2018-07-05 23:03:33 +0200

    Swapped MAL for AniList

--- NotRoboragi.py
+++ NotRoboragi.py
@@ -3,32 +3,60 @@ __copyright__ = "Copyright 2017"
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import re, pprint, configparser
-from Pymoe import Mal
+from Pymoe import Mal, Anilist
+import logging
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 conf = configparser.ConfigParser()
 conf.read("config")
 
-mal_username = conf['DEFAULT']['MAL_USERNAME']
-mal_password = conf['DEFAULT']['MAL_PASSWORD']
-
-instance = Mal(mal_username, mal_password)
+instance = Anilist()
 
 token = conf['DEFAULT']['TOKEN']
+
+url = {
+    'anime': "https://anilist.co/anime",
+    'manga': "https://anilist.co/manga"
+}
+
+
+def format_caption(url, medium):
+    english_title, romaji_title = '', ''
+
+    try:
+        english_title = medium['title']['english']
+    except KeyError:
+        pass
+    try:
+        romaji_title = medium['title']['romaji']
+    except KeyError:
+        pass
+
+    caption = "<b>{}</b>\n<i>{}</i>\n{}/{}/".format(romaji_title, english_title, url, medium['id'])
+
+    return caption
 
 
 def search(bot, update):
     temp_anime = re.search(r'\{\{(.*?)\}\}', update.message.text)
 
+    print(update)
+
     if temp_anime is not None:
-        anime = instance.anime.search(temp_anime.group(1))[0]
-        update.message.reply_text("https://myanimelist.net/anime/{}".format(anime.id))
+        anime = instance.search.anime(temp_anime.group(1))['data']['Page']['media'][0]
+
+        update.message.reply_photo(photo=anime['coverImage']['large'],
+                                   caption=format_caption(url['anime'], anime),
+                                   parse_mode="html")
         return
 
     temp_manga = re.search(r'\[\[(.*?)\]\]', update.message.text)
 
     if temp_manga is not None:
-        manga = instance.manga.search(temp_manga.group(1))[0]
-        update.message.reply_text("https://myanimelist.net/manga/{}".format(manga.id))
+        manga = instance.search.manga(temp_manga.group(1))['data']['Page']['media'][0]
+        update.message.reply_photo(photo=manga['coverImage']['large'],
+                                   caption=format_caption(url['manga'], manga),
+                                   parse_mode="html")
         return
 
 
